c50bb150ce318b0980520c149db2842249d77c73
Nico Gundel <nico@gun-del.de>
2020-11-22 15:43:43 +0100

    Version 2

--- src/Thread.java
+++ src/Thread.java
@@ -5,6 +5,7 @@ public class Thread extends java.lang.Thread {
     private Color color;
     @Override
     public void run() {
+        Robot r = new GTACasinoClicker().bot;
         do {
             try {
                 Thread.sleep(2);
@@ -12,21 +13,22 @@ public class Thread extends java.lang.Thread {
                 e.printStackTrace();
             }
             color = GTACasinoClicker.bot.getPixelColor(GTACasinoClicker.x, GTACasinoClicker.y);
-        } while (color.getRed() != color.getBlue() || color.getRed() != color.getGreen() || color.getRed() != 229);
+        } while (color.getRed() + color.getBlue() + color.getGreen() != 687);
         long time = System.nanoTime();
 
         try {
-            sleep(1400);
+            sleep(3950);
         } catch (InterruptedException e){
             e.printStackTrace();
             System.exit(-1);
         }
 
-        GTACasinoClicker.bot.keyPress(KeyEvent.VK_S);
-        GTACasinoClicker.bot.keyRelease(KeyEvent.VK_S);
+        r.keyPress(KeyEvent.VK_S);
+        r.keyRelease(KeyEvent.VK_S);
 
         double d = ((System.nanoTime() - time) / 1000000000.0);
         System.out.println(d + " Sekunden");
 
+        System.exit(0);
     }
 }
